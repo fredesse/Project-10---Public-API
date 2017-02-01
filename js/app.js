71956f1596854879bff5e52c8200bfa92e128c6e
Fredy-Edwin Esse <eleegia@gmail.com>
2017-01-31 16:14:25 -0800

    Cleaned everything up, validated code and checked cross-browser compatibility

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 var searchBtn = document.getElementById("search");
-var albumImage = document.querySelector(".album-img");
 var resultTitle = document.getElementById("resultsFor");
 
 // Searches for album artwork when button is pressed
 searchBtn.addEventListener("click", function (e) {
 	e.preventDefault();
-	var albums = document.getElementById("searchBar").value;
+	var albums = document.getElementById("searchBar").value; //Gets search value
 
-	$(".search-title").remove();
+	$(".search-title").remove(); // Clears previous search from title
 
+	// Displays search title
 	var artworkFor = document.createElement("p");
 	artworkFor.className = "search-title";
 	artworkFor.textContent = "Album artwork for " + albums;
 	resultTitle.appendChild(artworkFor);
 
+	// Requests data, loops through and displays it
 	var spotifyAPI = "https://api.spotify.com/v1/search";
 	var spotifyOpts = {
 		q: albums,
@@ -32,119 +33,15 @@ searchBtn.addEventListener("click", function (e) {
 	$.getJSON(spotifyAPI, spotifyOpts, searchAlbums);
 });
 
-$("#imageGallery").on("click", ".album-img", function(event) {
-	event.preventDefault();
-	console.log("I was clicked");
-
-	function getCurrentAlbum(currentAlbum) {
-		var imageLocation = $(currentAlbum).attr("href");
-		$image.attr("src", imageLocation);
-	};
-})
-
+// Search field clears when focused
 $("#searchBar").focus(function() {
 	$(this).val('');
 	$(this).attr("placeholder", "");
-})
+});
 
+// Puts placeholder back on off-focus
 $("#searchBar").focusout(function() {
-	if ($(this).val() == '') {
+	if ($(this).val() ==='') {
 		$(this).attr("placeholder", "Search...");
-	};
-})
-
-/*
-
-var $overlay = $('<div id="overlay"></div>');
-var $image = $('<img></img>');
-var $caption = $("<p></p>");
-var $title = $("<h1></h1>");
-
-var $nextArrow = $('<div id="nextArrow"></div>');
-var $prevArrow = $('<div id="prevArrow"></div>');
-
-$overlay.append($image);		// Adds image to overlay
-
-$image.before($prevArrow);		// Adds left arrow
-$image.after($nextArrow);		// Adds right arrow
-
-$overlay.append($title);
-$overlay.append($caption);
-
-
-$("body").append($overlay);   // Adds overlay to body
-
-
-$("#imageGallery a").click(function(event) {		// Captures the click event on a link
-	event.preventDefault();
-
-	$overlay.fadeTo('slow', 1).show();
-});
-
-
-$prevArrow.click(function(event) {				// Shows previous image when clicked
-	event.stopPropagation();
-	getPrevImage();
-});
-
-$nextArrow.click(function(event) {				// Shows next image when clicked
-	event.stopPropagation();
-	getNextImage();
-});
-
-
-document.body.addEventListener('keydown', function(event){					// Keyboard navigation for next picture
-	var key = event.keyCode || event.which;
-	if (key === 39) {
-		getNextImage();
 	}
 });
-
-document.body.addEventListener('keydown', function(event){					// Keyboard navigation for previous picture
-	var key = event.keyCode || event.which;
-	if (key === 37) {
-		getPrevImage();
-	}
-});
-
-
-function getCurrentImage (currentImage) {				// Gets current image
-	thisImage = currentImage;
-
-	var imageLocation = $(currentImage).attr("href");
-	$image.attr("src", imageLocation).attr("frameborder", "0").attr("scrolling", "no").attr("allowfullscreen", "allowfullscreen");
-
-	
-	var captionTitle = $(currentImage).attr("data-title");		// Sets the title
-	$title.text(captionTitle);
-	
-	var captionText = $(currentImage).children("img").attr("alt");			// Sets the captions 
-	$caption.text(captionText);
-}
-
-function getPrevImage() {									// Gets the previous image
-	imageParent = $(thisImage).prev();
-	if(imageParent.length!==0) {
-		thisImage = $(imageParent);
-	}
-	getCurrentImage(thisImage);
-}
-
-function getNextImage() {								// Gets the next image
-    imageParent = $(thisImage).next();
-    if(imageParent.length!==0) {
-    	thisImage = $(imageParent);
-    }
-    getCurrentImage(thisImage);
-}
-
-
-$overlay.click(function() {			// Hides overlay with click
-	$overlay.hide('slow');
-});
-
-$(document).keyup(function(e) {			// Hides overlay with ESC
-	if (e.keyCode == 27) {
-		$overlay.hide('slow');
-	}
-});*/
